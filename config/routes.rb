c7ace00abd3c5be89f47dbd60cfb8b64761baf1d
Kevin Hang <herezy2006@gmail.com>
2022-07-01 12:57:40 +0200

    modified Css // Generate controllers + views // Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "homes#index"
+
+  resources :contacts, only: [:index]
+  resources :abouts, only: [:index]
+  resources :faqs, only: [:index]
 end
